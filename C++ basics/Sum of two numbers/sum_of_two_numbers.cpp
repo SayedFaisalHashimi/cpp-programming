5a07503e96cc8b004e68c24efeb1606fc8f6946a
Sayed Faisal Hashimi <140006205+SayedFaisalHashimi@users.noreply.github.com>
2026-01-01 20:32:03 +0300

    Add program to calculate sum of two numbers
    Add C++ example for integer addition using cin and cout.

---
+++ C++ basics/Sum of two numbers/sum_of_two_numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, sum = 0;
+
+    cout << "Enter the first number: ";
+    cin >> a;
+
+    cout << "Enter the second number: ";
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "Sum is: " << sum << "\n";
+
+    return 0;
+}
